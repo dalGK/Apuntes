b7439ed16ddf2fb77ecf0ed41d11ef97653f740a
DaL <Dalvin20099@gmail.com>
2018-05-17 08:51:09 +0200

    Update cocheMasbarato.java

--- cocheMasbarato.java
+++ cocheMasbarato.java
@@ -1,12 +1,12 @@
- 	public static int cocheMasBarato(Vehiculo coches[]) { //En el metodo pedimos el Array[] coches Tipo Vehiculo
-		float precio;		  // Creamos variable precio
-		int indice = 0;		//Aqui guardamos el resultado
-						        	// Pedimos el precio de coches y lo recorremos
+public static int cocheMasBarato(Vehiculo coches[]) { //En el metodo pedimos el Array[] coches Tipo Vehiculo
+float precio;		  // Creamos variable precio
+int indice = 0;			//Aqui guardamos el resultado
+				// Pedimos el precio de coches y lo recorremos
                       
-		precio = coches[0].getPrecio();	    // Guardamos en la variable precio el precio del coche introducido en el array[0]
-		for(int i=1;i<coches.length;i++) {  // Creamos un bucle para recorrer todos los valores dentro del array
-		if(coches[i].getPrecio()<precio) {  // si el precio del coche[i] es < que precio actual
-		precio = coches[i].getPrecio();     // si es mas pequeño lo guarda en precio
+precio = coches[0].getPrecio();	    // Guardamos en la variable precio el precio del coche introducido en el array[0]
+for(int i=1;i<coches.length;i++) {  // Creamos un bucle para recorrer todos los valores dentro del array
+if(coches[i].getPrecio()<precio) {  // si el precio del coche[i] es < que precio actual
+precio = coches[i].getPrecio();     // si es mas pequeño lo guarda en precio
 				
 			}
 		}
